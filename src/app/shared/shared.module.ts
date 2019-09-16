1474f51ce392179fd1e822e26ab62378f6cf3c3c
alexanderbedoya82122 <alexander_bedoya82122@elpoli.edu.co>
2019-09-16 18:56:42 -0500

    primer

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CompartidoComponent } from './compartido/compartido.component';
+
+@NgModule({
+  declarations: [ CompartidoComponent],
+  imports: [
+    CommonModule,
+    CompartidoComponent
+
+  ]
+})
+export class SharedModule { }
